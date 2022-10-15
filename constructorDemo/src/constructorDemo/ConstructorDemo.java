b07a7846d07676726533e3768dd5830aef6ff73d
Enes <75938150+justedu16@users.noreply.github.com>
2022-10-15 19:41:13 +0300

    Notlar

---
+++ constructorDemo/src/constructorDemo/ConstructorDemo.java
@@ -0,0 +1,10 @@
+package constructorDemo;
+
+public class ConstructorDemo {
+
+	public static void main(String[] args) {
+		Product product = new Product(343, "tencere kapağı", 40.3);
+
+	}
+
+}
---
+++ constructorDemo/src/constructorDemo/Product.java
@@ -0,0 +1,10 @@
+package constructorDemo;
+
+public class Product {
+
+	public Product(int id, String description, double price) {
+		System.out.println("Constructor çalıştı.");
+
+	}
+
+}
